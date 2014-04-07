f9c823b19180e4276af03c78b203ab7cf3b19e87
alkogolikov <golikov.org@gmail.com>
2014-04-07 20:09:07 +0400

    Сделал поле - целое число

--- cms/reflex/theme/test.php
+++ cms/reflex/theme/test.php
@@ -3,6 +3,6 @@
 $user = new user();
 $field = mod::field("array");
 $field->setModel($user);
-$view = new \Infuso\Cms\Reflex\FieldView\ArrayField($field);
+$view = new \Infuso\Cms\Reflex\FieldView\numberView($field);
 $view->template()->exec();
 
