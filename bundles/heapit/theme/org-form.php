cbcb0c4c60e4e380e3c6fb1168fe2e5db306fdb3
alkogolikov <golikov.org@gmail.com>
2014-04-08 18:25:02 +0400

    Сделал сохранение контрагента

--- bundles/heapit/theme/org-form.php
+++ bundles/heapit/theme/org-form.php
@@ -5,42 +5,47 @@
     <table>
         <tr>
             <td>ФИО / Название</td>
-            <td><input type='text' name='title' value='{e($org->data("title"))}' ></td>
+            <td><input type='text' name='title' value='{e($org->data("title"))}' class='g-input-long' ></td>
         </tr>
         <tr>
             <td></td>
             <td>
-                helper("<input type='checkbox' name='org'  >")
-                    ->attr("checked",true)
-                    ->exec();
-                <label>Организация</label>                
+                $checkboxId = \util::id();
+                $checkbox = helper("<input id='{$checkboxId}' type='checkbox' name='person'  >");                    
+                if($org->data("person")) {
+                    $checkbox->attr("checked",true);
+                }
+                $checkbox->exec();
+                <label for='{$checkboxId}' >Частное лицо</label>                
             </td>
         </tr>
         <tr>
             <td>Телефон</td>
-            <td><input type='text' name='phone' ></td>
+            <td><input type='text' name='phone' value='{e($org->data("phone"))}' ></td>
         </tr>
         <tr>
             <td>Сайт</td>
-            <td><input type='text' name='url' ></td>
+            <td><input type='text' name='url' value='{e($org->data("url"))}' ></td>
         </tr>
         <tr>
             <td>Электропочта</td>
-            <td><input type='text' name='email' ></td>
+            <td><input type='text' name='email' value='{e($org->data("email"))}' ></td>
         </tr>
         <tr>
             <td>ICQ</td>
-            <td><input type='text' name='icq' ></td>
+            <td><input type='text' name='icq' value='{e($org->data("icq"))}' ></td>
         </tr>
         <tr>
             <td>Скайп</td>
-            <td><input type='text' name='skype' ></td>
+            <td><input type='text' name='skype' value='{e($org->data("skype"))}' ></td>
         </tr>        
         <tr>
             <td>Кто привел</td>
             <td>
                 $w = new \Infuso\Heapit\Widget\Autocomplete;
                 $w->fieldName("referral");
+                $w->value($org->data("referral"));
+                $w->title($org->pdata("referral")->title());
                 $w->serviceUrl("/infuso/heapit/controller/widget/orgList");
                 $w->exec();
             </td>
