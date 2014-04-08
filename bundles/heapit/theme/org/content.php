28ff0300c128fb5ac6c1a29ec551eabe566637b3
alkogolikov <golikov.org@gmail.com>
2014-04-08 18:41:15 +0400

    Набросок формочки сотрудников

--- bundles/heapit/theme/org/content.php
+++ bundles/heapit/theme/org/content.php
@@ -2,5 +2,6 @@
 
 <form class='o64rds6rlp' style='padding:30px;' data:orgid='{$org->id()}' >
     <h1 class='g-header' >Контрагент «{e($org->data("title"))}»</h1>
-    tmp::exec("/heapit/org-form");
+    exec("/heapit/org-form");
+    exec("staff");
 </form>
