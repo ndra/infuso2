0f0afda6412d2c03b1b0a1c0e0c2b9ef481f6ecf
Kopleman <kopleman@spark-mail.ru>
2014-04-18 14:14:55 +0400

    ops fix :D

--- bundles/heapit/theme/widgets/userchooser/user-select.php
+++ bundles/heapit/theme/widgets/userchooser/user-select.php
@@ -1,6 +1,6 @@
 <? 
 <div class="user-select-zaqfrsj6nf" >
-    foreach(user::all()->neq("userpick", "")->limit(0) as $user){
+    foreach(user::all()->neq("nickName", "")->limit(0) as $user){
         $str = $user->data("userpick");
         <div class="item" user:id="{$user->id()}">
             <img src="$str">
