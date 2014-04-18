c62ec51499a8c2797d9926f5b6257e0101574310
alkogolikov <golikov.org@gmail.com>
2014-04-18 18:51:50 +0400

    Делаю добавление ролей в админке

--- cms/user/theme/editor/manage/content/roles/ajax.php
+++ cms/user/theme/editor/manage/content/roles/ajax.php
@@ -1,9 +1,29 @@
 <? 
 
-<div class='zrtlp4pj2i' >
-    foreach($user->roles() as $role) {
-        <div>
-            echo $role->title();
-        </div>
-    }
+<div class='zrtlp4pj2i' data:userid='{$user->id()}'  >
+
+    <div class='roles' >
+        foreach($user->roles() as $role) {
+            <div class='role' >
+                echo $role->title();
+            </div>
+        }
+    </div>
+    
+    <input type='button' class='g-button add' value='Добавить роль' />   
+    
+    <div class='add-role-container' >
+    
+        <select class='g-input role-select' style='margin-right:10px;' >
+            $roles = \Infuso\User\Model\Role::all();
+            foreach($roles as $role) {
+                <option value='{$role->code()}' >{$role->title()}</option>
+            }
+        </select>
+        
+        <input type='button' class='g-button ok' value='Добавить' />
+        <input type='button' class='g-button cancel' value='Отмена' />
+    
+    </div>    
+    
 </div>
