5fefab4aeb6b27aff51dcbfde68ee699749ef5d7
alkogolikov <golikov.org@gmail.com>
2014-04-22 15:38:30 +0400

    Статьи доходов-расходов

--- bundles/heapit/theme/payment-form.js
+++ bundles/heapit/theme/payment-form.js
@@ -1,13 +1,26 @@
 $(function() {
+
     var form = $(".payment-vlj855earc");     
-    $(window).bind('keydown', function(event) {
+    $(document).on('keydown', function(event) {
         if (event.ctrlKey || event.metaKey) {
             switch (String.fromCharCode(event.which).toLowerCase()) {
-            case 's':
+                case 's':
                     event.preventDefault();
                     form.parent().submit();
-                break;
+                    break;
             }
         }
     });
+    
+    $(".payment-vlj855earc input[name=direction]").change(function() {
+        var value = $("input[name=direction]:checked").val();
+        if(value == "income") {
+            $(".payment-vlj855earc .group-income").removeAttr("disabled");
+            $(".payment-vlj855earc .group-expenditure").attr("disabled", true);
+        } else {
+            $(".payment-vlj855earc .group-expenditure").removeAttr("disabled");
+            $(".payment-vlj855earc .group-income").attr("disabled", true);
+        }
+    });
+    
 });       
