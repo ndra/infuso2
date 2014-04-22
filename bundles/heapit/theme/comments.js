f1e1d6581c574f406651b5d9f633e2af3e143f89
Kopleman <kopleman@spark-mail.ru>
2014-04-22 15:27:39 +0400

    loader в коммента�

--- bundles/heapit/theme/comments.js
+++ bundles/heapit/theme/comments.js
@@ -1,29 +1,17 @@
 $(function() {
-    /*$(".comments-ckvopjhgwq").mod("init",function() {
-        var container = $(this).find(".items");
-        var parent = $(this).attr("data:parent");
-        var updateComments = function() {
-            mod.call({
-                cmd: "infuso/heapit/controller/comments/list",
-                parent: parent
-            }, function(html) {
-                container.html(html);
-            });    
-        }
-        updateComments();
-        mod.on("comments/update-list", function() {
-            updateComments();    
-        });
-    });*/
     
     var load = function() {
+        var loader = $(".comments-ckvopjhgwq .loader");
+        var itemsList = $(".comments-ckvopjhgwq .items");
+        itemsList.html("");
+        loader.show();
         var data = {};
         mod.fire("beforeLoadCollection", data);
-        var itemsList = $(".comments-ckvopjhgwq .items");
         data.cmd = "Infuso/Heapit/Controller/Payment/search";
         data.parent = $(".comments-ckvopjhgwq").attr("data:parent");
         data.cmd = "Infuso/Heapit/Controller/Comments/search";   
         mod.call(data, function(data) {
+            loader.hide();
             itemsList.html(data.html);
             $(".comments-ckvopjhgwq .c-toolbar").trigger("collection-loaded", data);
         }, {
--- bundles/heapit/theme/comments.php
+++ bundles/heapit/theme/comments.php
@@ -7,6 +7,9 @@
     <div class='items' >        
     </div>
     
+    $loaderSrc = $this->bundle()->path()."/res/img/misc/loader.gif"; 
+    <img class="loader" src="{$loaderSrc}">
+    
     <div class='comment-container' >
         exec("addComment");
     </div>
