15e4878b225383544c8b351cf37b82a196034265
Alexander <goilikov.org@gmail.com>
2014-04-28 00:41:29 +0400

    Обновление по F5

--- bundles/board/theme/shared/task-list.js
+++ bundles/board/theme/shared/task-list.js
@@ -1,25 +1,26 @@
 $(function() {
 
     var load = function() {
-       // var loader = $(".task-list-layout-32tvfkepng .loader");
         var itemsList = $(".rpu80rt4m0 .ajax-container");
-        
-        //loader.show();
         var data = {};
-        //mod.fire("beforeLoadCollection", data);
         data.cmd = "Infuso/Board/Controller/Task/getTasks";
-        //data.status = $(".task-list-layout-32tvfkepng").attr("tasks:status")
         mod.call(data, function(data) {
             itemsList.html(data.html);
-            //$(".od655esqwh .c-toolbar").trigger("collection-loaded", data);
         }, {
             unique: "32tvfkepng"
         });
         
     }
     
-    //mod.on("load-task-list",load);
-    
     setTimeout(load,0);
+    
+    // При нажатии на F5 перегружаем список задач
+    
+    $(document).keydown(function(event) {
+        if(event.which === 116) {
+            load();
+            event.preventDefault();
+        }
+    });
 
 }); 
