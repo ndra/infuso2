e6a4c39980109217cfb42cd778dc24fa07f4a349
alkogolikov <golikov.org@gmail.com>
2014-05-06 05:32:10 +0400

    Починил сортировку

--- bundles/board/theme/shared/task-list/ajax.js
+++ bundles/board/theme/shared/task-list/ajax.js
@@ -2,6 +2,7 @@ $(function() {
    
     $(".task-list-zjnu1r2ofp").mod("init", function() {
         new Sortable(this, {
+            draggable: ".task",
             onUpdate: function() {
                 mod.msg("sort complete");
             }
