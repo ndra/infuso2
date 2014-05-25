e4d550a87f42bdca992854c21eaf0986a09988c4
alkogolikov <golikov.org@gmail.com>
2014-05-26 03:07:57 +0400

    Левое меню теперь не прыгает

--- cms/reflex/theme/layout/menu.js
+++ cms/reflex/theme/layout/menu.js
@@ -1,5 +1,18 @@
 $(function() {
 
+    // Метод, сохраняющий контент левого меню в хранилище сессии
+    var storeMenuHTML = function() {
+        var html = $(".rfgwepfkds").html();
+        sessionStorage.setItem("reflex/left-menu-html", html);
+    }
+    
+    setInterval(storeMenuHTML, 2000);
+    
+    // Восстанавливаем контент левого меню из хранилища сессии
+    // Далее этот контент будет заменен загруженным с сервера
+    $(".rfgwepfkds").html(sessionStorage.getItem("reflex/left-menu-html"));
+
+    // Возвращает список всех раскрытых нод
     var getExpandedNodes = function() {
         var data = sessionStorage.getItem("reflex/left-menu");
         if(data) {
@@ -8,6 +21,7 @@ $(function() {
         return [];
     }
 
+    // Делаем запрос сервер для получения контента меню
     mod.call({
         cmd:"infuso/cms/reflex/controller/menu/root",
         expanded: getExpandedNodes(),
@@ -15,5 +29,6 @@ $(function() {
     }, function(html) {
         $(".rfgwepfkds").html(html);
     });
+    
     
 });
