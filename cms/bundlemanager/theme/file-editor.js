4cf4a8dfccc4c60862de9e5fd71304732f5cfe80
alkogolikov <golikov.org@gmail.com>
2014-05-30 19:35:59 +0400

    ctrl+s в редакторе

--- cms/bundlemanager/theme/file-editor.js
+++ cms/bundlemanager/theme/file-editor.js
@@ -1,14 +1,30 @@
 mod.init(".y1esl75wqs", function() {
 
+    var $container = $(this);
+
     var id = $(this).find(".editor").attr("id");
     var editor = ace.edit(id);
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/php");
     
+    // При ресайзе окна запускаем отложенеый ресайз редактора
+    // @todo переписать по-человечески
     $(window).resize(function() {
         setTimeout(function() {
             editor.resize();
         });
+    });
+    
+    $(this).mod("on","keydown", function(e) {
+    
+        if(!$container.filter(":visible").length) {
+            return;
+        }
+    
+        if(e.which == 83 && e.ctrlKey) {
+            mod.msg(editor.getValue());
+            e.preventDefault();
+        }
     });
     
 });
