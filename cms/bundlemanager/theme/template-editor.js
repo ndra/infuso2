052d026453f834050416cec0b40765ece13e9107
alkogolikov <golikov.org@gmail.com>
2014-06-04 23:01:02 +0400

    esc в редакторе

--- cms/bundlemanager/theme/template-editor.js
+++ cms/bundlemanager/theme/template-editor.js
@@ -1,12 +1,14 @@
 mod.init(".aivh9q8neu", function() {
 
     var $container = $(this);
+    var $tabs = $container.find(".tabs").children();
+    var $tabsContent = $container.find(" > .center").children();
     
     $container.layout();
 
     var selectTab = function(n) {
     
-        $container.find(".top").children().each(function(i) {
+        $tabs.each(function(i) {
             if(i==n) {
                 $(this).addClass("active");
             } else {
@@ -14,32 +16,49 @@ mod.init(".aivh9q8neu", function() {
             }
         });
         
-        $container.find(".center").children().each(function(i) {
+        $tabsContent.each(function(i) {
             if(i==n) {
                 $(this).show();
             } else {
                 $(this).hide();
             }
         });
+        
+        var id = $tabsContent.filter(".editor:visible").attr("id");
+        ace.edit(id).focus();
     
     };
     
     selectTab(0);
     
-    $container.find(".top").children().each(function(n) {
+    $tabs.each(function(n) {
         $(this).click(function() {
             selectTab(n);
         });
     });
     
-    $container.find(".center").children().each(function(n) {
+    $container.find(".top .functions").click(function() {
+        var id = $tabsContent.filter(".editor:visible").attr("id");
+        var makeid = function() {
+        
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        
+            for( var i=0; i < 10; i++ )
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+        
+            return text;
+        }
+        ace.edit(id).insert(makeid());
+    });
+    
+    $tabsContent.each(function(n) {
 
         var id = $(this).attr("id");
         var editor = ace.edit(id);
         var $editor = $(this);
         editor.setTheme("ace/theme/monokai");
         editor.getSession().setMode("ace/mode/" + $editor.attr("data:lang"));
-        
 
         $editor.on("layoutchange", function() {
             editor.resize();
@@ -50,6 +69,11 @@ mod.init(".aivh9q8neu", function() {
        
             if(!$editor.filter(":visible").length) {
                 return;
+            }
+            
+            if(e.which == 27) {
+                var id = $tabsContent.filter(".editor:visible").attr("id");
+                ace.edit(id).focus();
             }
         
             if(e.which == 83 && e.ctrlKey) {
--- cms/bundlemanager/theme/template-editor.php
+++ cms/bundlemanager/theme/template-editor.php
@@ -2,11 +2,21 @@
 
 <div class='aivh9q8neu' data:theme='{get_class($template->theme())}' data:template='{$template->relName()}'>
 
-    <div class='top' >
-        <div>php</div>
-        <div>js</div>
-        <div>css</div>
-    </div>
+    <table class='top' >
+        <tr>
+            <td class='tabs' >
+                <div>php</div>
+                <div>js</div>
+                <div>css</div>
+            </td>
+            <td class='functions' >
+                echo "id";
+            </td>
+        </tr>
+    </table>
+        
+        <div style='float:right;' >id</div>
+    </table>
     <div class='center' >
         <div id='x{\util::id()}' class='editor layout-change-listener' data:type='php' data:lang='php' >{e($template->contents("php"))}</div>
         <div id='x{\util::id()}' class='editor layout-change-listener' data:type='js'  data:lang='javascript' >{e($template->contents("js"))}</div>
