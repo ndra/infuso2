bec5969108debaf1022f851cd7d4f4f8186254e3
alkogolikov <golikov.org@gmail.com>
2014-07-03 20:06:01 +0400

    Убрал старое поведение

--- mod/activerecord/class/record.php
+++ mod/activerecord/class/record.php
@@ -100,6 +100,56 @@ abstract class Record extends \Infuso\Core\Model\Model {
 		}
     }
     
+	/**
+	 * @return Функция должна вернуть url объекта
+	 * По умолчанию, url объекта имеет вид /my_class_name/item/id/123
+	 * Переопределите функцию, если у элемента должен быть другой url
+	 **/
+	public function recordUrl() {
+		return null;
+	}
+
+	/**
+	 * @return Функция должна вернуть родительский элемент
+	 * Это должен быть существующий или не существующий объект reflex, либо null
+	 * Родитель используется в каталоге для построения пути к объекту
+	 **/
+	public function recordParent() {
+		return reflex::get("reflex_none",0);
+	}
+	
+	public function recordTitle() {
+
+        if(!$this->exists()) {
+			return "";
+		}
+
+		if($field = $this->recordTitleField()) {
+        	if($title = $this->data($field)){
+	            return $title;
+	        }
+        }
+
+        return get_class($this).":".$this->id();
+    }
+
+    public function recordTitleField() {
+        // перебираем поля до первого поля с именем title
+        foreach($this->fields() as $field) {
+            if($field->name()=="title") {
+                return $field->name();
+            }
+        }
+        // перебираем поля до первого поля сторокогвого типа и возвращаем его имя
+        foreach($this->fields() as $field){
+            if($field->typeID() == "v324-89xr-24nk-0z30-r243"){
+                return $field->name();
+            }
+        }
+
+        return false;
+    }
+    
     /**
      * Триггер, вызывается перед созданием
      **/
@@ -135,6 +185,18 @@ abstract class Record extends \Infuso\Core\Model\Model {
      **/
     public function afterDelete() {
 	}
+	
+	/**
+	 * Триггер, вызывающийся перед каждой поперацией создания, изменения или удаления
+	 **/
+	public function beforeOperation() {
+	}
+
+	/**
+	 * Триггер, вызывающийся после каждой поперацией создания, изменения или удаления
+	 **/
+	public function afterOperation() {
+	}
 
     public function __sleep() {
         return array(
@@ -159,12 +221,6 @@ abstract class Record extends \Infuso\Core\Model\Model {
 		    $method($collection);
 		}
 
-    }
-
-    public function defaultBehaviours() {
-        $ret = parent::defaultBehaviours();
-        $ret[] = "reflex_defaultBehaviour";
-        return $ret;
     }
 
     public static function classes() {
