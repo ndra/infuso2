e79d511cba8c99265bee8aa04cf481e73e458835
ivandra <ivan@ndra.ru>
2014-07-21 18:03:21 +0400

    Замена tmp: часть 12

--- cms/bundlemanager/class/controller/files.php
+++ cms/bundlemanager/class/controller/files.php
@@ -14,21 +14,15 @@ class Files extends Core\Controller {
 	}
 	
 	public function post_right($p) {
-        return \tmp::get("/bundlemanager/files-right")
-            ->param("bundle", service("bundle")->bundle($p["bundle"]))
-            ->getContentForAjax();
+		return app()->tm("/bundlemanager/files-right")->param("bundle",service("bundle")->bundle($p["bundle"]))->getContentForAjax();
 	}
     
 	public function post_list($p) {
-        return \tmp::get("/bundlemanager/files-right/nodes")
-            ->param("path", \file::get($p["path"]))
-            ->getContentForAjax();
+		return app()->tm("/bundlemanager/files-right/nodes")->param("path", \file::get($p["path"]))->getContentForAjax();
 	}
     
     public function post_editor($p) {
-        return \tmp::get("/bundlemanager/file-editor")
-            ->param("path", \file::get($p["path"]))
-            ->getContentForAjax();
+		return app()->tm("/bundlemanager/file-editor")->param("path", \file::get($p["path"]))->getContentForAjax();
     }
 
 }
