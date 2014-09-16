4afdb37785a162968db1b59ce0a2709a8942604e
alkogolikov <golikov.org@gmail.com>
2014-09-16 17:21:38 +0400

    Делаю загружалку 1с

---
+++ bundles/eshop1c/class/behaviour/item.php
@@ -0,0 +1,73 @@
+<?
+
+namespace Infuso\Eshop1C\Behaviour;
+use \Infuso\Core;
+use \Infuso\Eshop1C;
+
+/**
+ * Поведение для товарной позиции, содержащее все необходимое для интеграции с 1С
+ **/ 
+class Item extends Core\Behaviour {
+
+    public function addToClass() {
+        return "infuso\\eshop\\item";
+    }
+
+    public function behaviourPriority() {
+        return -1;
+    }
+
+    /**
+     * Дополнительные поля для управления импортом
+     **/
+    public function fields() {
+        return array(
+            mod_field::get("textfield")->name("importKey")->disable()->label("1С: Внешний ключ")->group("1C"),
+            //mod_field::get("datetime")->name("importTime")->disable()->label("1С: Время испорта")->group("1C"),
+            //mod_field::get("textfield")->name("importCycle")->disable()->label("1С: Цикл импорта")->group("1C"),
+        );
+    }
+
+    /**
+     * Метод, обрабатывающий товар из файла import.xml
+     * В этом файле приходят описания товаров и групп
+     **/
+    public function processCatalogXML($towar, $xml) {
+
+        $importKey = $towar->Ид."";
+        $data = array(
+            "title" => $towar->Наименование."",
+            "importKey" => $importKey,
+            "article" => $towar->Артикул."",
+            "description" => $towar->Описание."",
+            "order" => true,
+        );
+        
+        // Загружаем группу
+        $groupID = $towar->Группы->Ид."";
+        $groupXML = end($xml->xpath("//Классификатор/Группы/descendant::Группа[Ид='$groupID']"));
+        
+        if($groupXML) {
+	        $vgroup = service("ar")->virtual("eshop_group");
+	        $group = $vgroup->processCatalogXML($groupXML,$xml);
+	        $data["parent"] = $group->id();
+		}
+        
+        $item = Eshop1C\Utils::importItem($data);
+    }
+
+    /**
+     * Метод, обрабатывающий товар из файла offers.xml
+     * В этом файле приходит информация о ценах на товарные позиции
+     * (сами товары загружаются в import.xml)
+     **/
+    public function processOffersXML($offer,$xml) {
+        $data = array(
+            "price" => end($offer->xpath("descendant::ЦенаЗаЕдиницу"))."",
+            "instock" => $offer->Количество."",
+            "importKey" => $offer->Ид."",
+        );
+        Eshop1C\Utils::importItem($data);
+    }
+
+}
---
+++ bundles/eshop1c/class/controller/exchange.php
@@ -0,0 +1,235 @@
+<?
+
+namespace Infuso\Eshop1C\Controller;
+use \Infuso\Core;
+
+/**
+ * Контроллер импорта товаров
+ * 1С Подаключается к этому классу для обмена товарами и заказами
+ **/
+class Exchange extends Core\Controller {
+
+	/**
+	 * Каталог, к оторый будут записываться файлы импорта
+	 **/
+    private static $dir = "/eshop/import/";
+    
+    /**
+     * Количество позиций, испортируемых за один шаг
+     **/
+    private static $step = 20;
+
+	/**
+	 * Разрешаем работу контроллеру
+	 **/
+    public static function indexTest() {
+        return true;
+    }
+    
+    public function controller() {
+        return "1c";
+    }
+
+    /**
+     * Единый контроллер обмена с 1С
+     **/
+    public function index() {
+    
+        $cmd = "$_GET[type]:$_GET[mode]";
+
+        $dir = self::$dir;
+        $user = $_SERVER["PHP_AUTH_USER"];
+        $password = $_SERVER["PHP_AUTH_PW"];
+        $_user = $this->param("login");
+        $_password = $this->param("password");
+
+        if(!$_user || !$_password) {
+            service("log")->log(array(
+				"message" => "Не задан логин и пароль для соедиения с 1С",
+				"type" => "1c/exchange"
+			));
+            return;
+        }
+
+        if($user!=$_user || $password!=$_password) {
+            service("log")->log(array(
+				"message" => "Логин и пароль ($user:$password), переданные 1С не совпадают с указанными на сайте.",
+				"type" => "1c/exchange"
+			));
+            return;
+        }
+
+        service("log")->log(array(
+			"message" => "Логин и пароль ($user:$password), переданные 1С не совпадают с указанными на сайте.",
+			"type" => "1c/exchange"
+		));
+		
+        mod::trace("$cmd");
+
+        switch($cmd) {
+
+            case "sale:checkauth":
+            case "catalog:checkauth":
+                echo "success\n";
+                break;
+
+            // Начало выгрузки на сайт
+            case "catalog:init":
+                file::mkdir($dir);
+                eshop_1c_utils::importBegin();
+                self::from(0);
+                echo "zip=no\n";
+                echo "file_limit=0\n";
+                break;
+
+            // Прием файла
+            case "catalog:file":
+                $str = file_get_contents("php://input");
+                $file = file::get("$dir/$_GET[filename]");
+                file::mkdir($file->up());
+                $file->put($str);
+
+                // Сохраняем имя последнего переданного файла
+                // Это понадобится нам позже чтобы определить когда закончить выгрузку
+                file::get("{$dir}/last-import-file.txt")->put($_GET["filename"]);
+
+                echo "success";
+                break;
+
+            // Разбор файла
+            case "catalog:import":
+
+                $filename = $_GET["filename"];
+
+                if(preg_match("/import/",$filename)) {
+
+                    if(self::importCatalog($filename)) {
+                        echo "success";
+                        die();
+                    } else {
+                        echo "progress\n";
+                        echo self::from();
+                        die();
+                    }
+                }
+
+                if(preg_match("/offers/",$filename)) {
+                    if(self::importOffers($filename)) {
+
+                        if($filename == file::get("{$dir}/last-import-file.txt")->data()) {
+                            eshop_1c_utils::importComplete();
+                            mod::trace("1c export done");
+                        }
+
+                        echo "success";
+                        die();
+                    } else {
+                        echo "progress\n";
+                        echo self::from();
+                        die();
+                    }
+                }
+
+                break;
+
+            // Начало обмена заказами
+            case "sale:init":
+                echo "zip=no\n";
+                echo "file_limit=0\n";
+                break;
+
+            // 1с запрашивает заказы у сайта
+            case "sale:query":
+                header("Content-type: application/xml");
+                $xml = self::saleXML();
+                $xml = mb_convert_encoding($xml->asXML(),"cp-1251","utf-8");
+                echo $xml;
+                break;
+
+            // 1с отправляет заказы на сайт. Пока ничего не делаем с этим
+            case "sale:file":
+                echo "success";
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /**
+     * @return Возвращает xml с заказами с сайта
+     * Этот xml будет отправлен в 1C
+     **/
+    public function saleXML() {
+        $xml = simplexml_load_string("<КоммерческаяИнформация ВерсияСхемы='2.03' ДатаФормирования='".util::now()->notime()."' />");
+        $parent = $xml;
+
+        // Выгружаем заказы за последние 90 дней
+        $days = 90;
+        $orders = eshop_order::all()->neq("status",0)->eq("1CExportCompleted",0)->limit(20);
+
+        foreach($orders as $order) {
+            $document = $parent->addChild("Документ");
+            $order->export1CXML($document);
+            $order->data("1CExportCompleted",true);
+        }
+
+
+
+        return $xml;
+    }
+
+    public static function from($from = 0) {
+
+        if(func_num_args()==0) {
+            return file::get(self::$dir."/step.txt")->data();
+        }
+
+        if(func_num_args()==1){
+            return file::get(self::$dir."/step.txt")->put($from);
+        }
+    }
+
+    public static function importCatalog($filename = "import.xml") {
+
+        $vitem = reflex::virtual("eshop_item");
+
+        $xml = simplexml_load_file(file::get(self::$dir."/".$filename)->native());
+        $items = $xml->xpath("//Каталог/Товары/Товар");
+        $count = sizeof($items);
+        $from = self::from();
+        $to = $from+self::$step;
+        $items = $xml->xpath("//Каталог/Товары/Товар[position()>=$from and position()<=$to]");
+
+        foreach($items as $towar) {
+            $vitem->processCatalogXML($towar,$xml);
+        }
+
+        self::from($to);
+        if($to>=$count) {
+            self::from(0);
+            return true;
+        }
+    }
+
+    public static function importOffers($filename = "offers.xml") {
+
+        $vitem = reflex::virtual("eshop_item");
+
+        $xml = simplexml_load_file(file::get(self::$dir."/".$filename)->native());
+        $items = $xml->xpath("//Предложение");
+        $count = sizeof($items);
+        $from = self::from();
+        $to = $from+self::$step;
+        $items = $xml->xpath("//Предложение[position()>=$from and position()<=$to]");
+        foreach($items as $offer) {
+            $vitem->processOffersXML($offer,$xml);
+        }
+        self::from($to);
+        if($to>=$count) {
+            self::from(0);
+            return true;
+        }
+    }
+
+}
