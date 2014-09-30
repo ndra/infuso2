fd8e21fdcf6f44c3f3519214cb94a8485d328769
ivandra <ivan@ndra.ru>
2014-09-30 09:51:44 +0400

    Убрал Type number у поля

--- cms/reflex/theme/fields/integer.php
+++ cms/reflex/theme/fields/integer.php
@@ -3,7 +3,7 @@
 <div class='bk3fvoznwu' >
 
     if($field->editable()) {
-        <input name='{$field->name()}' value='{$field->value()}' type='number' />
+        <input name='{$field->name()}' value='{$field->value()}' />
     } else {
         <input value='{$value}' disabled />
     }
