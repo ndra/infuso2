2348e0eb95c5b5eb533e9b14a034750e4c9a453b
alkogolikov <golikov.org@gmail.com>
2014-11-13 22:13:37 +0300

    доска / сохранение ника

--- bundles/board/theme/conf/content/nick.js
+++ bundles/board/theme/conf/content/nick.js
@@ -3,7 +3,7 @@ $(function() {
     $(".iv09v31m1i").submit(function(e) {
         e.preventDefault();
         mod.call({
-            cmd:"infuso/heapit/controller/conf/save",
+            cmd:"infuso/board/controller/conf/save",
             data: $(".iv09v31m1i").mod("formData")
         });
     });
