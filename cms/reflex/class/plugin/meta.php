6b608f6fc1193b691a003c33e6ab397ae35f782e
Kopleman <kopleman@spark-mail.ru>
2014-12-15 16:01:06 +0300

    запись мет для обьектов

--- cms/reflex/class/plugin/meta.php
+++ cms/reflex/class/plugin/meta.php
@@ -19,13 +19,46 @@ class Meta extends \Infuso\Core\Component {
         $item = $this->component();
         return \Infuso\Cms\Reflex\Model\Meta::get(get_class($item).":".$item->id());
 	}
-    
+
+    public function getOrCreateMetaObject() {
+        $item  = $this->metaObject();
+        if(!$item->exists()){
+            $item = $this->create();
+        }
+        return $item;
+    }
+
+    /** 
+     * @todo сделать какойто агрегатор для установки/получения всех мет за раз  [Алексюшка]
+     **/
+
+    public function title() {
+        $item  = $this->getOrCreateMetaObject();
+        $args = func_get_args();
+        if(count($args)){
+            $title = $args[0];
+            $item->data("title", $title);
+        }
+        return $item->data("title");
+    }
+
+    public function head() {
+        $item  = $this->getOrCreateMetaObject();
+        $args = func_get_args();
+        if(count($args)){
+            $title = $args[0];
+            $item->data("head", $title);
+        }
+        return $item->data("head");
+    }
+
     public function create() {
         $item = $this->component();
         $hash = get_class($item).":".$item->id();
         $meta = service("ar")->create("\\Infuso\\Cms\\Reflex\\Model\\Meta", array(
-            "hash" => get_class($item).":".$item->id(),
+            "hash" => $hash,
         ));
+        return $meta;
     }
     
     public function factory() {
