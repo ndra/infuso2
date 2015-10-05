c33d9b44b69e6ef322563bf8eb4f5e01a88f58d6
ivandra <ivan@ndra.ru>
2015-10-05 14:04:00 +0300

    правка для поля link

--- cms/reflex/theme/fields/link.php
+++ cms/reflex/theme/fields/link.php
@@ -2,7 +2,11 @@
 $editorInfo = get_class($editor).":".$editor->itemId();  
 <div class='x4qRpzOJXG' data:editor='{$editorInfo}' data:field='{$field->name()}'>
     <input type='hidden' name='{$field->name()}' value='{$field->value()}' />
-    <span class='title' >{$field->pvalue()->title()}</span>
+	$title = $field->pvalue()->title();
+	if ($title == ""){
+		$title = "пусто";   	
+	}
+    <span class='title' >{$title}</span>
     
     if($field->value()){
         $editor = \Infuso\CMS\Reflex\Editor::get($editorInfo);
