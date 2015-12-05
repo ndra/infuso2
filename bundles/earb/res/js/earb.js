b810fba2b467866152068e57f38ec2ba828ac2dc
alkogolikov <golikov.org@gmail.com>
2015-12-05 15:15:49 +0300

    earb

--- bundles/earb/res/js/earb.js
+++ bundles/earb/res/js/earb.js
@@ -1,15 +1,102 @@
-window.earb = new function() {
+window.earb = function(params) {
 
-    var audioContext = new window.AudioContext();
-    var earb = this;       
-    this.soundEnabled = true;
+    this.params = params;
     
+    var extend = function(obj, extend) {
+        if(!obj) {
+            obj = {};
+        }
+        for(var i in extend) {
+            if(obj[i] === undefined) {
+                obj[i] = extend[i];
+            }
+        }
+        return obj;
+    }
+    
+    this.params = extend(this.params, {
+        bpm: 120,
+        timeSignature: [4,4],
+    });
+    
+    var earb = this;   
+
+    this.audioContext = new window.AudioContext();        
+    this.soundEnabled = true;
+
+    this.frames = [];
+    var tick32 = 0;
+    
+    var scale = window.earb.scales.minor(0);
+    
+    this.instruments = [];
+    
+    var interval = null;
+
+    this.frame = function(time, callback) {
+        this.frames[time] = callback;
+    }
+    
+    /**
+     * Обработчик каждой 1/32 ноты
+     **/         
+    this.handle32 = function() { 
+        var n = 32 / earb.params.timeSignature[1] * earb.params.timeSignature[0];           
+        
+        var timeEvent = {
+            tick : tick32,
+            barTick: tick32 % n,
+            bar: Math.floor(tick32 / n),
+        }
+            
+        if(tick32 % n == 0 ) {
+            earb.handleBar(timeEvent);
+        }
+        if(tick32 % (32 / earb.params.timeSignature[1]) == 0 ) {
+            // Доля. Пока ничего не делаем с этим                    
+        }
+        
+        for(var i in earb.instruments) {
+            earb.instruments[i].handle32(timeEvent);
+        }
+        
+        tick32 ++;
+    }
+    
+    /**
+     * Обрабатывает начало такта
+     **/                         
+    this.handleBar = function(event) {
+        var fn = earb.frames[event.bar];
+        if(fn) {
+            fn.apply(earb);
+        }
+    }
+    
+    /**
+     * Проигрывает композицию
+     **/         
+    this.play = function() {
+    
+        tick32 = 0;
+        
+        // Длительность 1/32 ноты 
+        var duration4 = 60 / 120;
+        var duration32 = duration4 / 8;
+        
+        interval = setInterval(this.handle32, duration32 * 1000);
+        
+    }   
+    
+    this.stop = function() {
+        clearInterval(interval);
+    }      
+  
     /**
      * Возвращает частоту ноты
      **/         
     this.getNoteFrequency = function(note) {
-        note += 48;
-        note -= 12;
+        note += 36;
         var frequency = 27.5 * Math.pow(2, note / 12);
         return frequency;
     }
@@ -17,145 +104,179 @@ window.earb = new function() {
     /**
      * Создает новый инструмент
      **/         
-    this.instrument = function() { 
-        return new function() {
-        
-            this.params = {};
-            this.params.monophonic = false;
-        
-            var maxVoices = 10;
-            var voices = [];
-            var instrument = this;
-            
-            for(var i = 0; i < maxVoices; i ++) {
-                voices[i] = new function() {                
-                    this.amp = audioContext.createGain();
-                    this.amp.connect(audioContext.destination);
-                    this.amp.gain.value = 0;
-                    this.oscillator = audioContext.createOscillator();
-                    this.oscillator.connect(this.amp);
-                    this.oscillator.type = 'sawtooth';
-                    this.oscillator.start(0);
-                }();
-            }
-            
-            /**
-             * Возвращает первый свободный голос
-             **/                         
-            this.getFreeVoice = function() {
-                for(var i = 0; i < maxVoices; i ++) {
-                    if(!voices[i].playing) {
-                        return voices[i];
-                    }                
-                }
-                alert("Too many voices");
-            }
-        
-            /**
-             * Проигрывает заданную ноту
-             **/         
-            this.playNote = function(p1,p2) {
-            
-                if(!earb.soundEnabled) {
-                    return;
-                }
-                
-                var signature = (typeof p1)+":"+(typeof p2);
-                
-                switch(signature) {
-                    case "object:undefined":
-                        params = p1;
-                        break;
-                    case "number:number":
-                        params = {
-                            note:p1,
-                            duration: p2
-                        };
-                        break;
-                    default:                    
-                        alert("interument.playNode bad params " + signature);
-                        break;
-                }
-            
-                if(!params) {
-                    params = {
-                        duration: 1000,
-                        note: 0
-                    };
-                }
-            
-                if(instrument.params.monophonic) {
-                    var voice = voices[0];
-                    voice.oscillator.frequency.value = earb.getNoteFrequency(params.note);
-                    voice.amp.gain.value = .3;
-                } else {
-                    var voice = this.getFreeVoice(); 
-                    voice.oscillator.frequency.setValueAtTime(earb.getNoteFrequency(params.note),0);
-                    voice.amp.gain.value = .3;
-                    voice.playing = true;
-                    setTimeout(function() {
-                        voice.amp.gain.value = 0;
-                        voice.playing = false;
-                    }, params.duration);
-                }
-            }
-            
-            this.pattern = function(p, scale) {
-                return new function() {
-                
-                    this.params = p;    
-                
-                    if(!this.params.speed) {
-                        this.params.speed =  125;
-                    }
-                    if(!this.params.shift) {
-                        this.params.shift = 0;
-                    }
-                                                  
-                    var patternObject = this;  
-                    var iterator = 0;           
-                    var onStep = function() {};
-                    this.play = function() {
-                        setInterval(function() {
-                            patternObject.step()
-                        }, this.params.speed);
-                    };
-                    this.step = function() {
-                    
-                        if(iterator % this.params.pattern.length == 0) {
-                            onStep.apply(this, [Math.floor(iterator / this.params.pattern.length)]);
-                            this.params.shift += this.params.shiftNext;
-                        }  
-                    
-                        var patternStep = this.params.pattern[iterator % this.params.pattern.length] * 1;
-                        instrument.playNote({
-                            note: scale.note(patternStep + this.params.shift),
-                            duration: this.params.speed
-                        });                           
-                        iterator ++;    
-                   
-                    };  
-                    
-                    this.setPattern = function(pattern) {
-                        this.params.pattern = pattern;
-                    }
-                    
-                    this.shift = function(s) {
-                        this.params.shift = s;
-                    }
-                    
-                    this.onStep = function(fn) {
-                        onStep = fn;
-                    }
-                                    
-                }();
-            };
-            
-        }();
+    this.instrument = function(params) { 
+        return new window.earb.instrument(this,params);
     } 
     
+    this.tick32 = function() {
+        return tick32;
+    }
+    
+    /**
+     * Возвращает гамму (тональность) песни
+     **/         
+    this.scale = function() {
+        return scale;
+    }
+    
+
+    
+};
+
+// ----------------------------------------------------------------------------- Инструмент
+
+earb.instrument = function(song, params) {
+        
+    song.instruments.push(this);
+        
+    this.params = {};
+    this.params.monophonic = false;
+    
+    var patternObject = null;
+
+    var maxVoices = 10;
+    var voices = [];
+    var instrument = this;
+    
+    // Создаем голоса
+    for(var i = 0; i < maxVoices; i ++) {
+        voices[i] = new function() {                
+            this.amp = song.audioContext.createGain();
+            this.amp.connect(song.audioContext.destination);
+            this.amp.gain.value = 0;
+            this.oscillator = song.audioContext.createOscillator();
+            this.oscillator.connect(this.amp);
+            this.oscillator.type = 'sawtooth';
+            this.oscillator.start(0);
+        }();
+    }
+    
+    /**
+     * Возвращает первый свободный голос
+     **/                         
+    this.getFreeVoice = function() {
+        for(var i = 0; i < maxVoices; i ++) {
+            if(!voices[i].playing) {
+                return voices[i];
+            }                
+        }
+        alert("Too many voices");
+    }
+
+    /**
+     * Проигрывает заданную ноту
+     **/         
+    this.playNote = function(p1,p2) {
+    
+        if(!song.soundEnabled) {
+            return;
+        }
+        
+        var signature = (typeof p1)+":"+(typeof p2);
+        
+        switch(signature) {
+            case "object:undefined":
+                params = p1;
+                break;
+            case "number:number":
+                params = {
+                    note:p1,
+                    duration: p2
+                };
+                break;
+            default:                    
+                alert("interument.playNode bad params " + signature);
+                break;
+        }              
+  
+        if(instrument.params.monophonic) {
+            var voice = voices[0];
+            voice.oscillator.frequency.value = song.getNoteFrequency(params.note);
+            voice.amp.gain.value = .3;
+        } else {
+            var voice = this.getFreeVoice(); 
+            voice.oscillator.frequency.setValueAtTime(song.getNoteFrequency(params.note),0);
+            voice.amp.gain.value = .3;
+            voice.playing = true;
+            setTimeout(function() {
+                voice.amp.gain.value = 0;
+                voice.playing = false;
+            }, params.duration);
+        }
+    }
+    
+    /**
+     * Обработчик 32 ноты
+     **/                         
+    this.handle32 = function(tick) {
+        if(patternObject) {
+            patternObject.handle32(tick);    
+        }
+    }         
+    
+    this.pattern = function(params) {
+        patternObject = new window.earb.pattern(this, params);
+        return patternObject;
+    }       
+    
+    this.song = function() {
+        return song;
+    }     
+          
+}
+
+// ----------------------------------------------------------------------------- Паттерн
+
+earb.pattern = function(instrument, params) {
+
+    var pattern = {
+        1: {stage: 1, duration: 60},
+        2: {stage: 2, duration: 60},
+        3: {stage: 3, duration: 60},
+        4: {stage: 4, duration: 60},
+        5: {stage: 5, duration: 60},
+        6: {stage: 6, duration: 60},
+    };
+    
+    var startTick = null; // Тик, от которого считать
+    
+    var stepDuration = 16; // Длительность шага ( 16 означает 1/16, 8 означает 1/8 и т.д.)
+    
+    var numberOfSteps = 6; // Количество шагов в паттерне
+    
+    this.handle32 = function(event) {
+    
+        var tick = event.tick - startTick;  
+
+        // Только ноты паттерна
+        if(tick % (32 / stepDuration) != 0) {
+            return;
+        } 
+        
+        var step = (tick / (32 / stepDuration)) % numberOfSteps;        
+        this.handleStep(step);
+    }
+    
+    this.handleStep = function(step) {
+        var data = pattern[step + 1];
+        if(data) {
+            var note = instrument.song().scale().note(data.stage);
+            instrument.playNote(note,data.duration);
+        }
+    }
+    
+    this.start = function() {
+        startTick = instrument.song().tick32();
+    }   
+    
+}
+
+// ----------------------------------------------------------------------------- Гаммы
+
+earb.createScales = function() {
+
     // Создаем гаммы
-    this.scales = {};
+    earb.scales = {};
     
     var scaleSchemes = {
         minor: [1, 3, 4, 6, 8, 9, 11],
@@ -165,7 +286,7 @@ window.earb = new function() {
     };
     
     for(var name in scaleSchemes) {
-        this.scales[name] = function(tonic) {
+        earb.scales[name] = function(tonic) {
             return new function() {
                 var steps = scaleSchemes[name];
                 this.note = function(step) {
@@ -176,62 +297,6 @@ window.earb = new function() {
             }();
         };
     }
+};
+earb.createScales();
     
-    // -------------------------------------------------------------------------
-    // Композиция
-    
-    this.song = function() {    
-        return new function() {
-        
-            var song = this;
-        
-            this.params = {
-                bpm: 120,
-                timeSignature: [4,4], // Размер
-            };
-        
-            this.frames = [];
-            this.tick32 = 0;
-        
-            this.frame = function(time, callback) {
-                this.frames[time] = callback;
-            }
-            
-            this.handle32 = function() { 
-                var n = 32 / song.params.timeSignature[1] * song.params.timeSignature[0];               
-                if(song.tick32 % n == 0 ) {
-                    song.handleBar(song.tick32 / n);
-                }
-                if(song.tick32 % (32 / song.params.timeSignature[1]) == 0 ) {
-                    // Доля. Пока ничего не делаем с этим                    
-                }
-                song.tick32 ++;
-            }
-            
-            /**
-             * Обрабатывает начало такта
-             **/                         
-            this.handleBar = function(counter) {
-                var fn = song.frames[counter];
-                if(fn) {
-                    fn.apply(song);
-                }
-            }
-            
-            this.play = function() {
-            
-                song.tick32 = 0;
-                
-                // Длительность 1/32 ноты 
-                var duration4 = 60 / 120;
-                var duration32 = duration4 / 8;
-                
-                setInterval(this.handle32, duration32 * 1000);
-                
-            }
-        
-        }();
-        
-    };
-
-}();
--- bundles/earb/theme/index.js
+++ bundles/earb/theme/index.js
@@ -1,27 +1,23 @@
 $(function() {
     
-    var bass = earb.instrument("bass");
-    /*var minor = earb.scales.minor(0);
-    var pattern = bass.pattern({
-        pattern: [1,2,3,4],
-    }, minor);
-    pattern.play(); */
+    var song = new earb();
+    var bass = song.instrument("bass");
 
-    var song = earb.song();
     song.frame(0,function() {
         mod.msg("Ололо! Начало песня");
-        bass.playNote(1,500);
+        
+       // bass.playNote(1,500);
     });
     song.frame(1,function() {
         mod.msg("Первый фрейм!!!!");
-        bass.playNote({
-            note: 10,
-            duration: 500
-        });
+        bass.pattern("Жопа жопа жопа жопа").start();
+
     });
     song.frame(3,function() {
         mod.msg("Третий фрейм!!!!");
     });
     song.play();
+    
+    $(window).blur(song.stop);
 
 });
