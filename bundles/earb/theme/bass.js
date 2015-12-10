eac28385d62ba8a7bf70c8d90215a3aeecc478c0
alkogolikov <golikov.org@gmail.com>
2015-12-10 22:30:42 +0300

    eaeb

--- bundles/earb/theme/bass.js
+++ bundles/earb/theme/bass.js
@@ -1,32 +1,21 @@
 $(function() {
     
-    var song = new earb();
-    song.scale(earb.scales.xminor());
-    var bass1 = song.instrument("bass");
+    var song = new earb({
+        bpm: 140
+    });
+    song.scale(earb.scales.minor(-1));
+    var bass1 = song.instrument("sine");
     var bass2 = song.instrument("bass");
     var fantasia = song.instrument("fantasia").gain(.5);
     
     song.frame(0,function() {
+    
         
-        fantasia.pattern("1 . . .    . . . .   . . . .   . . . . ");
-        fantasia.onbar(function(event) {
-            this.degree([0,3][event.bar % 2])
-        });
-        
-        bass1.pattern("1 1 8 1 9 8 1 1 . 1 . . 1 . 1 .");
+        bass1.pattern("0 . 7 . 8 9 8 .  7 . 8 .  9 . 7 . ");
         bass1.onbar(function(event) {
-            this.degree([0,3][event.bar % 2])
+            //this.degree([0,3][event.bar % 2])
         });
-        
-        bass2.pattern("1 -6 2 -5 3 -4");
-        bass2.onbar(function(event) {
-            this.degree([0,3][event.bar % 2] + 7 * 3 )
-        });
-        
-        /*bass2.pattern("1 1 8 1 9 8 1 1 . 1 . . 1 . 1 .");
-        bass2.onbar(function(event) {
-            this.degree([0,2,-3,-4][event.bar % 4])
-        }); */
+
         
     });
     
