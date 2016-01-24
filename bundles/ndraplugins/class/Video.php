c62121de2256bad16a2861c2edadaa46ac68de34
alkogolikov <golikov.org@gmail.com>
2016-01-24 21:49:18 +0300

    Превью видео

--- bundles/ndraplugins/class/Video.php
+++ bundles/ndraplugins/class/Video.php
@@ -11,14 +11,23 @@ class Video {
         $this->src = $src;
     } 
     
-    public function player($width = 560, $height = 315) {
-        
+    public function player($width = 560, $height = 315) {         
         if(preg_match("/http(s)?\:\/\/www\.youtube\.com/", $this->src)){
             $url = \Infuso\Core\Url::get($this->src);
             $id = $url->query("v");
-            return "<iframe width='{$width}' height='{$height}' src='http://www.youtube.com/embed/{$id}' frameborder='0' allowfullscreen></iframe>";
-        }
-        
+            return "<iframe width='{$width}' height='{$height}' src='http://www.youtube.com/embed/{$id}' frameborder='0' allowfullscreen ></iframe>";
+        }         
+    }
+    
+    /**
+     * Возвращает превью видео
+     **/         
+    public function thumbnail() {
+        if(preg_match("/http(s)?\:\/\/www\.youtube\.com/", $this->src)){
+            $url = \Infuso\Core\Url::get($this->src);
+            $id = $url->query("v");
+            return "http://img.youtube.com/vi/{$id}/hqdefault.jpg";
+        } 
     }
 
 }
