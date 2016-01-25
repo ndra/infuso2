55d8c34d191040363b903f60811bc8761be4a5a7
alkogolikov <golikov.org@gmail.com>
2016-01-25 09:39:30 +0300

    Исправил некорректное отображение поля int

--- cms/reflex/theme/fields/integer.php
+++ cms/reflex/theme/fields/integer.php
@@ -3,7 +3,7 @@
 <div class='bk3fvoznwu' >
 
     if($field->editable()) {
-        <input name='{$field->name()}' value='e({$field->value()})' />
+        <input name='{$field->name()}' value='{e($field->value())}' />
     } else {
         <input value='e({$field->value()})' disabled />
     }
