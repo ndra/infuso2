21f183438c3e650f605e57a0f71bdb521196de87
alkogolikov <golikov.org@gmail.com>
2016-02-01 14:19:10 +0300

    Исправил глюк в поле с числом

--- cms/reflex/theme/fields/integer.php
+++ cms/reflex/theme/fields/integer.php
@@ -2,10 +2,22 @@
 
 <div class='bk3fvoznwu' >
 
-    if($field->editable()) {
+    /*if($field->editable()) {
         <input name='{$field->name()}' value='{e($field->value())}' />
     } else {
-        <input value='e({$field->value()})' disabled />
+        <input value='{e($field->value())}' disabled />
+    } */
+    
+    $w = widget("\\infuso\\cms\\ui\\widgets\\textfield")
+        ->value($field->value())  
+        ->style("width", 80)
+        ->fieldName($field->name());
+        
+    if(!$field->editable()) {
+        $w->attr("disabled", true);
     }
+        
+    $w->exec();
+        
     
 </div>
