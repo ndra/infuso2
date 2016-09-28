d70ada609ac31f5b8a9369697cf37f395a6a8efe
ivandra <ivan@ndra.ru>
2016-09-28 12:32:19 +0300

    Tiny / маркер

--- bundles/tinymce/theme/layout.js
+++ bundles/tinymce/theme/layout.js
@@ -48,6 +48,40 @@ mod.init(".tinyMCE-qPe4r8Zov0h", function() {
             "insertdatetime media nonbreaking save table contextmenu directionality",
             "emoticons template paste textcolor colorpicker textpattern"
         ],
+		style_formats: [
+			{title: 'Headers', items: [
+			  {title: 'Header 1', format: 'h1'},
+			  {title: 'Header 2', format: 'h2'},
+			  {title: 'Header 3', format: 'h3'},
+			  {title: 'Header 4', format: 'h4'},
+			  {title: 'Header 5', format: 'h5'},
+			  {title: 'Header 6', format: 'h6'}
+			 ]},
+			 {title: 'Inline', items: [
+			  {title: 'Bold', icon: 'bold', format: 'bold'},
+			  {title: 'Italic', icon: 'italic', format: 'italic'},
+			  {title: 'Underline', icon: 'underline', format: 'underline'},
+			  {title: 'Strikethrough', icon: 'strikethrough', format: 'strikethrough'},
+			  {title: 'Superscript', icon: 'superscript', format: 'superscript'},
+			  {title: 'Subscript', icon: 'subscript', format: 'subscript'},
+			  {title: 'Code', icon: 'code', format: 'code'}
+			 ]},
+			 {title: 'Blocks', items: [
+			  {title: 'Paragraph', format: 'p'},
+			  {title: 'Blockquote', format: 'blockquote'},
+			  {title: 'Div', format: 'div'},
+			  {title: 'Pre', format: 'pre'}
+			 ]},
+			 {title: 'Alignment', items: [
+			  {title: 'Left', icon: 'alignleft', format: 'alignleft'},
+			  {title: 'Center', icon: 'aligncenter', format: 'aligncenter'},
+			  {title: 'Right', icon: 'alignright', format: 'alignright'},
+			  {title: 'Justify', icon: 'alignjustify', format: 'alignjustify'}
+			 ]},
+			 {title: 'Маркеры списка', items: [
+			  {title: 'Custom Bullet',selector: 'ul', classes: 'tiny-marker-line'},
+			 ]}
+		],
         file_browser_callback : myFileBrowser,
         image_advtab: true,
         setup : function(editor) {
